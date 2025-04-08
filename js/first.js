0d7b747dc70eb54f7d7fa61ce53c4c40613f149e
Gyanendra Singh <gyanendrasingh6780@gmail.com>
2025-04-08 17:47:25 +0530

    change image

--- js/first.js
+++ js/first.js
@@ -33,3 +33,6 @@ window.addEventListener('scroll', () => {
 document.getElementById('menu-toggle').addEventListener('click', () => {
   document.getElementById('mobile-menu').classList.toggle('hidden');
 });
+
+
+
